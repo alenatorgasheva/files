a6e89c12211cd90af5470c0124211791e33df469
alenatorgasheva <in-fa@mail.ru>
2020-02-26 22:37:59 +0700

    Tasks.

--- main.py
+++ main.py
@@ -1,6 +1,58 @@
 # Case - study #7
-# This program
+# This program shows information about filing system.
 
 # Developers : Daniel A.         (%),
 #              Zemtseva A.       (%),
 #              Torgasheva A.     (%).
+
+# Tasks:
+#  - локализация, комментарии, код ревью, тестирование
+
+# Ready tasks:
+#  - выход
+
+import os
+
+
+def acceptCommand():
+    # ввод команды и одобрение ее
+    pass
+
+
+def runCommand(command):
+    pass
+
+
+def moveUp():
+    pass
+
+
+def moveDown(currentDir):
+    pass
+
+
+# TODO (Алина)
+def countFiles(path):
+    pass
+
+
+# TODO (Настя)
+def countBytes(path):
+    pass
+
+
+# TODO (Алёна)
+def findFiles(target, path):
+    pass
+
+
+def main():
+    MENU = ''
+    while True:
+        print(os.getcwd())
+        print(MENU)
+        command = acceptCommand()
+        runCommand(command)
+        if command == QUIT:
+            print('Работа программы завершена.')
+            break
