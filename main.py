5261dd5778511fa1f6c203eedf1dcdba0ccd9470
Настя <a.zemtseva@g.nsu.ru>
2020-02-28 18:17:25 +0700

    done.

--- main.py
+++ main.py
@@ -28,8 +28,18 @@ def acceptCommand():
 
 def runCommand(command):
     if command == '1':
-        print(os.listdir(os.getcwd()))
+        def list_files(startpath):
+            for root, dirs, files in os.walk(startpath):
+                if dir != '.git':
+                    level = root.replace(startpath, '').count(os.sep)
+                    indent = ' ' * 4 * (level)
+                    print('{}{}/'.format(indent, os.path.basename(root)))
+                    subindent = ' ' * 4 * (level + 1)
+                    for f in files:
+                        print('{}{}'.format(subindent, f))
 
+        startpath = os.getcwd()
+        list_files(startpath)
     elif command == '2':
         moveUp()
 
